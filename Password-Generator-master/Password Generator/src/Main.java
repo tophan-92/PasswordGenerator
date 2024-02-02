4d9a40f501254dec9f2415077e956a337d9135f9
prajn <prajn@LAPTOP-80M0JHJ0>
2024-02-02 16:37:55 +0530

    new change

--- Password-Generator-master/Password Generator/src/Main.java
+++ Password-Generator-master/Password Generator/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static final Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Generator generator = new Generator(keyboard);
+        Generator generator = new Generator(keyboard); //new object created
         generator.mainLoop();
         keyboard.close();
     }
